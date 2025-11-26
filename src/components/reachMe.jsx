f819dbb196ca474462496ec88350b05f11986ac0
Serge07cloud <wisdomandy.5@gmail.com>
2025-11-26 22:38:57 +0000

    Update section title

--- src/components/reachMe.jsx
+++ src/components/reachMe.jsx
@@ -3,7 +3,7 @@ const ReachMe = () =>
   return (
     <>
       <div className="text-center mx-auto max-w-5xl pb-48 sm:pb-48 lg:pb-48">
-        <h1 className="text-5xl font-bold tracking-tight text-balance dark:text-white sm:text-7xl">Ready to build <span className="bg-clip-text text-transparent bg-linear-to-r from-indigo-500 via-purple-500 to-blue-500">something Amazing</span>
+        <h1 className="text-5xl font-bold tracking-tight text-balance dark:text-white sm:text-7xl">Do you have <span className="bg-clip-text text-transparent bg-linear-to-r from-indigo-500 via-purple-500 to-blue-500 block">a project in mind ?</span>
         </h1>
         <div className="text-lg font-normal py-5 text-pretty max-w-3xl mx-auto text-gray-400 sm:text-xl/8">
           I'm currently open to new opportunities and collaborations. Whether you have a project in mind, a question, or just want to say hi, my inbox is always open. I'll try my best to get back to you!
